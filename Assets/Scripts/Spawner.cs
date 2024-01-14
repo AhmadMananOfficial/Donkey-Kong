e0a4fc92f0009064a60391e3e810057421199ada
AhmadMananOfficial <ahmadmananofficial@gmail.com>
2024-01-14 19:11:38 +0500

    Update

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+	public GameObject barrelPrefab;
+	public float minSpawnTime = 2.0f;
+	public float maxSpawnTime = 4.0f;
+    
+    void Start()
+    {
+	    Spawn();   
+    }
+
+    
+	void Spawn()
+    {
+	    Instantiate(barrelPrefab, transform.position, Quaternion.identity);
+	    Invoke(nameof(Spawn), Random.Range(minSpawnTime, maxSpawnTime));
+    }
+}
